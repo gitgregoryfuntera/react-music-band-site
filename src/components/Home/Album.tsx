25ec0a6e2a15fd60a1951d642088d72cf5273b65
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-10-23 20:33:24 +0800

    - added initial animations

--- src/components/Home/Album.tsx
+++ src/components/Home/Album.tsx
@@ -1,9 +1,30 @@
+import { motion, Variants } from "framer-motion";
 import CLASSES from "./Album.module.scss";
 
 const Album = () => {
+  const variants: Variants = {
+    offscreen: {
+      opacity: 0,
+      y: 100,
+    },
+    onscreen: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        ease: "linear",
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <section className={CLASSES.root}>
-      <h2>New Album</h2>
+      <motion.h2
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{ once: true, amount: 0.8 }}
+        variants={variants}
+      >New Album</motion.h2>
       <div className={CLASSES.spotifyEmbed}>
         <iframe
           src="https://open.spotify.com/embed/album/0SgxFNwplcpKZTSRhh2Snh?utm_source=generator"
